6c5b740821158a9a42800e39f4446b1ae1cc5922
BraydenJMassa <BraydenJMassa@gmail.com>
2025-10-27 19:42:04 -0400

    fixed url's for deployment

--- client/src/hooks/useRefresh.ts
+++ client/src/hooks/useRefresh.ts
@@ -27,37 +27,3 @@ const useRefresh = (
 }
 
 export default useRefresh
-
-// import axios from 'axios'
-// import { useAuth } from './useAuth'
-// import { useState } from 'react'
-
-// const useRefresh = () => {
-//   const { setAuth } = useAuth()
-//   const [loading, setLoading] = useState(false)
-//   const refresh = async () => {
-//     setLoading(true)
-//     try {
-//       const response = await axios.post(
-//         '/api/auth/refresh',
-//         {},
-//         {
-//           withCredentials: true,
-//         }
-//       )
-//       setAuth({
-//         accessToken: response.data.accessToken,
-//         userId: response.data.userId,
-//       })
-//       return response.data.accessToken
-//     } catch (err) {
-//       setAuth({ userId: '', accessToken: '' })
-//       return null
-//     } finally {
-//       setLoading(false)
-//     }
-//   }
-//   return { refresh, loading }
-// }
-
-// export default useRefresh
