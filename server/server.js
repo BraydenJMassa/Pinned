7b0eef97bc26fa82eb74705e91dc3300d4a070df
BraydenJMassa <BraydenJMassa@gmail.com>
2025-10-28 02:09:31 -0400

    removed slash from cors link

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ import { authCheck } from './middlewares/authCheck.js'
 const app = express()
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://pinned-roan.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://pinned-roan.vercel.app'],
     credentials: true,
   })
 )
