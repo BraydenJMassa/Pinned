0d104e865a65e118b23c46bfe51420a53dd51097
BraydenJMassa <BraydenJMassa@gmail.com>
2025-10-28 00:20:09 -0400

    Added enter functionality to ConfirmationModal

--- client/src/components/ConfirmationModal.tsx
+++ client/src/components/ConfirmationModal.tsx
@@ -1,13 +1,13 @@
-import { useEffect } from 'react'
-import '../styles/Modal.css'
+import { useEffect } from "react";
+import "../styles/Modal.css";
 
 type ConfirmationModalProps = {
-  title: string
-  confirmText?: string
-  cancelText?: string
-  onConfirm: () => void
-  onCancel: () => void
-}
+  title: string;
+  confirmText?: string;
+  cancelText?: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+};
 
 const ConfirmationModal = ({
   title,
@@ -16,36 +16,39 @@ const ConfirmationModal = ({
 }: ConfirmationModalProps) => {
   const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget && onCancel) {
-      onCancel()
+      onCancel();
     }
-  }
+  };
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Escape' && onCancel) {
-        onCancel()
+      if (e.key === "Escape" && onCancel) {
+        onCancel();
       }
-    }
-    window.addEventListener('keydown', handleKeyDown)
-    return () => window.removeEventListener('keydown', handleKeyDown)
-  }, [onCancel])
+      if (e.key === "Enter") {
+        onConfirm();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [onCancel]);
   return (
-    <div className='modal-backdrop' onClick={handleBackdropClick}>
-      <div className='modal' onClick={(e) => e.stopPropagation()}>
-        <button className='modal-x-btn' onClick={onCancel}>
+    <div className="modal-backdrop" onClick={handleBackdropClick}>
+      <div className="modal" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-x-btn" onClick={onCancel}>
           x
         </button>
-        <h1 className='modal-title'>{title}</h1>
-        <div className='modal-btns'>
-          <button className='modal-btn green-btn' onClick={onConfirm}>
+        <h1 className="modal-title">{title}</h1>
+        <div className="modal-btns">
+          <button className="modal-btn green-btn" onClick={onConfirm}>
             Yes
           </button>
-          <button className='modal-btn red-btn' onClick={onCancel}>
+          <button className="modal-btn red-btn" onClick={onCancel}>
             No
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ConfirmationModal
+export default ConfirmationModal;
