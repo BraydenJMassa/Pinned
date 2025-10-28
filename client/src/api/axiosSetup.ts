a82e2ffb676e2d32c7ec952ecd1a0b1367ed82b6
BraydenJMassa <BraydenJMassa@gmail.com>
2025-10-28 02:00:14 -0400

    updated url's for new deployment

--- client/src/api/axiosSetup.ts
+++ client/src/api/axiosSetup.ts
@@ -1,38 +1,37 @@
-import axios from "axios";
-import useRefresh from "../hooks/useRefresh";
-import { useEffect } from "react";
-import { useAuth } from "../hooks/useAuth";
+import axios from 'axios'
+import useRefresh from '../hooks/useRefresh'
+import { useEffect } from 'react'
+import { useAuth } from '../hooks/useAuth'
 
-// CHANGEME
 axios.defaults.baseURL = import.meta.env.PROD
-  ? "https://todo-app-u9wh.onrender.com/"
-  : "http://localhost:4000";
-axios.defaults.withCredentials = true;
+  ? 'https://pinned-l4nt.onrender.com'
+  : 'http://localhost:4000'
+axios.defaults.withCredentials = true
 
 const useAxiosInterceptor = () => {
-  const { setAuth } = useAuth();
-  const { refresh } = useRefresh(setAuth);
+  const { setAuth } = useAuth()
+  const { refresh } = useRefresh(setAuth)
 
   useEffect(() => {
     const interceptor = axios.interceptors.response.use(
       (res) => res,
       async (err) => {
-        const originalRequest = err.config;
+        const originalRequest = err.config
         if (err.response?.status === 401 && !originalRequest._retry) {
-          originalRequest._retry = true;
-          const newToken = await refresh();
+          originalRequest._retry = true
+          const newToken = await refresh()
           if (newToken) {
-            originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
-            return axios(originalRequest);
+            originalRequest.headers['Authorization'] = `Bearer ${newToken}`
+            return axios(originalRequest)
           }
         }
-        return Promise.reject(err);
+        return Promise.reject(err)
       }
-    );
+    )
 
     // cleanup to prevent duplicate interceptors
-    return () => axios.interceptors.response.eject(interceptor);
-  }, [refresh]);
-};
+    return () => axios.interceptors.response.eject(interceptor)
+  }, [refresh])
+}
 
-export default useAxiosInterceptor;
+export default useAxiosInterceptor
